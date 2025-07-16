d7bc0c05a9fdf828adf88b69d6a7bc6c365bd94f
Kittipat13 <kittipat.li@ku.th>
2025-07-16 16:02:12 +0700

    Add files via upload

--- lib/CartItem.java
+++ lib/CartItem.java
@@ -1,10 +1,3 @@
 public record CartItem(String sku, String name, double price, int quantity){
 
-    public CartItem get(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
-    }
-
-    
-
 }
